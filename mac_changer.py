8b923c2c91f1830e4ff80950277a8f1c3a718410
ilyes <ilyesdhiaeddine@gmail.com>
2021-04-30 10:48:15 -0400

    regex

--- mac_changer.py
+++ mac_changer.py
@@ -1,7 +1,16 @@
 #!usr/bin/env pyhton
-
 import subprocess
 import optparse
+import re
+
+
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[+] Could not read MAC address.")
 
 
 def get_arguments():
@@ -18,11 +27,18 @@ def get_arguments():
 
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
-
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
 
 Options = get_arguments()
+current_mac = get_current_mac(Options.interface)
+print("Current MAC ==> " + str(current_mac))
 change_mac(Options.interface, Options.new_mac)
+current_mac = get_current_mac(Options.interface)
+if current_mac == Options.new_mac:
+    print("[+] MAC address was successfully changed to " + current_mac)
+else:
+    print("[+] MAC address did not change ")
+
